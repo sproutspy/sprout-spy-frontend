c10c0866803f4392383de1edf6f077b26636aff4
Devstashio <guayando1207@gmail.com>
2024-03-03 09:20:25 -0500

    tried

--- src/app/new-plant/page.jsx
+++ src/app/new-plant/page.jsx
@@ -43,6 +43,35 @@ const NewPlantPage = () => {
         }
         
         // adds plantID,  plantName and user_id to the plants table in the database
+        
+        // if the plantID already has a id in the database, it will not add the plant, and will return an error
+        if (plantID === '' || plantName === '') {
+            alert('Plant ID and Plant Name must be filled out');
+            return;
+        }
+
+        if(user === null) {
+            alert('User not found');
+            router.push('/sign-in');
+            return;
+        }
+
+
+        supabase.from('plants').select().eq('id', plantID).then((response) => {
+            // checks to see if the user.id is the same as the owner_id in the database
+            let plant = response.data[0];
+            while (plant !== undefined && plant.owner_id !== user.id) {
+                if (plant.owner_id === user.id) {
+                    alert('Plant ID already exists');
+                    return;
+                }
+            }
+
+        }
+        ).catch((error) => {
+            console.error('Error checking plant ID: ', error);
+        });
+
         supabase.from('plants').insert([{ id: plantID, owner_id: user.id, plant_name: plantName }]).then((response) => {
             console.log(response);
             router.push('/dashboard');
@@ -65,10 +94,9 @@ const NewPlantPage = () => {
                     // @ts-ignore
                     align={"center"} borderRadius={"10px"} bgColor={"gray.700"} minW={{ md: "25rem", sm: "20rem", base: "40%" }} paddingX={"20px"} paddingY={"50px"}>
                     <Text fontSize={"5xl"} fontWeight={"bolder"} color={"gray.300"}>Add New Plant</Text>
-                    <Input placeholder="Plant Name" value={plantName} onChange={(e) => setPlantName(e.target.value)} />
-                    <Input placeholder="Plant ID" value={plantID} onChange={(e) => setPlantID(e.target.value)} />
-                    <Button onClick={onSubmit}>Submit</Button>
-
+                    <Input placeholder="Plant Name" value={plantName} onChange={(e) => setPlantName(e.target.value)} style={{ backgroundColor: "white", borderRadius: "5px", paddingLeft: "5px", paddingTop: "2px", paddingBottom: "2px" }} my={"10px"}/>
+                    <Input placeholder="Plant ID" value={plantID} onChange={(e) => setPlantID(e.target.value)} style={{ backgroundColor: "white", borderRadius: "5px", paddingLeft: "5px", paddingTop: "2px", paddingBottom: "2px" }} my={"10px"}/>
+                    <Button colorScheme={"green"} onClick={onSubmit}>Submit</Button>
                 </Box>
             </Flex>
         )
